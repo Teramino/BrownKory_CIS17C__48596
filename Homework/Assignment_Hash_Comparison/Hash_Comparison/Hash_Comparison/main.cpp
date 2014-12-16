451954d37dfaa7be945892a6c21228e1223b635f
Kory Brown <kjbrown123@gmail.com>
2014-12-16 02:48:40 -0800

    Small updates to hw

---
+++ Homework/Assignment_Hash_Comparison/Hash_Comparison/Hash_Comparison/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hash_Comparison
+//
+//  Created by Kory Brown on 12/15/14.
+//  Copyright (c) 2014 Kory Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
